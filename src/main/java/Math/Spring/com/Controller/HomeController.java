e5b4f909746136d3e5a2736d786a154bd716440b
Leedaero <ayoldr@hanmail.net>
2019-03-19 16:43:12 +0900

    템플릿 추가

--- src/main/java/Math/Spring/com/Controller/HomeController.java
+++ src/main/java/Math/Spring/com/Controller/HomeController.java
@@ -1,19 +1,48 @@
 package Math.Spring.com.Controller;
 
+import Math.Spring.com.DAO.ChartRepository;
+import Math.Spring.com.VO.Chart;
+import Math.Spring.com.VO.Doughnut;
+import Math.Spring.com.VO.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    ChartRepository rep;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(){
-
-
-
+    public String index() {
 
         return "index";
     }
+
+
+    @RequestMapping(value = "chartregist", method = RequestMethod.GET)
+    @ResponseBody
+    public String chartregist(Chart chart) {
+        System.out.println(chart);
+        if(rep.regist(chart) == 1) {
+            return "T";
+        }
+        else return "N";
+    }
+
+    @RequestMapping(value = "reserch", method = RequestMethod.GET)
+    @ResponseBody
+    public Doughnut reserch(Student student) {
+        System.out.println(student);
+
+        Doughnut doughnut = rep.reserchchart(student);
+        System.out.println(doughnut);
+
+        return doughnut;
+    }
+
 
 }
