4c599a357bdc606f15af11e9bbcaea6fd31017b5
hyunungLim <fkfmzm77@naver.com>
2019-03-19 15:17:49 +0900

    cdn_JS 추가

---
+++ web/resources/cdn_address.js
@@ -0,0 +1,4 @@
+//vueJS
+document.write("<script src='https://unpkg.com/vue'></script>");
+//jquery
+document.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>");
